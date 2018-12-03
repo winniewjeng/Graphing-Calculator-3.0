a5d58853d7c30872c8a84b8b97e4f9ff86fbe58c
winniewjeng <winniewjeng@gmail.com>
2018-12-03 11:51:06 -0800

    pulled up window

--- Calculator3.0/main.cpp
+++ Calculator3.0/main.cpp
@@ -1,15 +1,33 @@
-//
-//  main.cpp
-//  Calculator3.0
-//
-//  Created by Winnie Jeng on 12/3/18.
-//  Copyright © 2018 Winnie Jeng. All rights reserved.
-//
 
+#include "Interface.hpp"
+#include "ShuntingYard.hpp"
 #include <iostream>
+#include <string>
+#include "Interface.hpp"
+#include <SFML/Graphics.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+
+int main(int, char const**)
+{
+//    sf::RenderWindow window;
+//    window.create(sf::VideoMode(1920,1080), "test");
+//    window.setFramerateLimit(1);
+//    while (window.isOpen()) {
+//        sf::Event event;
+//        while (window.pollEvent(event)) {
+//            if (event.Closed) {
+//                window.close();
+//            }
+//        }
+//        window.clear();
+//
+//        window.display();
+//    }
+    Interface p;
+    p.run();
+//////    run();
+//    test_shunting();
     return 0;
 }
