5bfdca555d1e6bb59a9bae02399af19570801949
winniewjeng <winniewjeng@gmail.com>
2018-12-03 12:02:36 -0800

    tinkered and updated

--- Calculator3.0/main.cpp
+++ Calculator3.0/main.cpp
@@ -4,27 +4,12 @@
 #include <iostream>
 #include <string>
 #include "Interface.hpp"
-#include <SFML/Graphics.hpp>
-
 
 using namespace std;
 
 int main(int, char const**)
 {
-//    sf::RenderWindow window;
-//    window.create(sf::VideoMode(1920,1080), "test");
-//    window.setFramerateLimit(1);
-//    while (window.isOpen()) {
-//        sf::Event event;
-//        while (window.pollEvent(event)) {
-//            if (event.Closed) {
-//                window.close();
-//            }
-//        }
-//        window.clear();
-//
-//        window.display();
-//    }
+
     Interface p;
     p.run();
 //////    run();
