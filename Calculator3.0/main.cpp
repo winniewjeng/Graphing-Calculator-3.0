8c990f35d21e978ff73e66610d0d9b855627c962
winniewjeng <winniewjeng@gmail.com>
2018-12-03 11:21:52 -0800

    Initial Commit

---
+++ Calculator3.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator3.0
+//
+//  Created by Winnie Jeng on 12/3/18.
+//  Copyright © 2018 Winnie Jeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
